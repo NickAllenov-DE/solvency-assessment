e71846a90d49df494866bea79fc8f8855774b0a0
NickAllenov <AllenovNS@gmail.com>
2025-02-02 01:27:15 +0300

    Final version with working MLFlow

--- init.sql
+++ init.sql
@@ -1,31 +1,41 @@
+-- Создаем базу данных, если её нет
+SELECT 'CREATE DATABASE airflow'
+WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'airflow')\gexec
 
--- Создаем базу и пользователя для Airflow
+SELECT 'CREATE DATABASE mlflow'
+WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'mlflow')\gexec
+
+SELECT 'CREATE DATABASE superset'
+WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'superset')\gexec
+
+-- Создаем пользователей, если их нет
 DO $$
 BEGIN
-    IF NOT EXISTS (SELECT 1 FROM pg_database WHERE datname = 'airflow') THEN
-        CREATE DATABASE airflow;
+    IF NOT EXISTS (SELECT FROM pg_roles WHERE rolname = 'airflow') THEN
         CREATE USER airflow WITH PASSWORD 'airflow';
-        GRANT ALL PRIVILEGES ON DATABASE airflow TO airflow;
     END IF;
-END $$;
-
--- Создаем базу и пользователя для MLflow
-DO $$
-BEGIN
-    IF NOT EXISTS (SELECT 1 FROM pg_database WHERE datname = 'mlflow') THEN
-        CREATE DATABASE mlflow;
+    IF NOT EXISTS (SELECT FROM pg_roles WHERE rolname = 'mlflow') THEN
         CREATE USER mlflow WITH PASSWORD 'mlflow';
-        GRANT ALL PRIVILEGES ON DATABASE mlflow TO mlflow;
     END IF;
-END $$;
-
--- Создаем базу и пользователя для Superset
-DO $$
-BEGIN
-    IF NOT EXISTS (SELECT 1 FROM pg_database WHERE datname = 'superset') THEN
-        CREATE DATABASE superset;
+    IF NOT EXISTS (SELECT FROM pg_roles WHERE rolname = 'superset') THEN
         CREATE USER superset WITH PASSWORD 'superset';
-        GRANT ALL PRIVILEGES ON DATABASE superset TO superset;
     END IF;
 END $$;
 
+-- Даем права на базы данных
+GRANT ALL PRIVILEGES ON DATABASE airflow TO airflow;
+GRANT ALL PRIVILEGES ON DATABASE mlflow TO mlflow;
+GRANT ALL PRIVILEGES ON DATABASE superset TO superset;
+
+-- Даем полные права на схему public для каждого пользователя
+\c mlflow
+GRANT USAGE, CREATE ON SCHEMA public TO mlflow;
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON TABLES TO mlflow;
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON SEQUENCES TO mlflow;
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT ALL ON FUNCTIONS TO mlflow;
+
+\c airflow
+GRANT USAGE, CREATE ON SCHEMA public TO airflow;
+
+\c superset
+GRANT USAGE, CREATE ON SCHEMA public TO superset;
